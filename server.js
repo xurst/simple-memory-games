06fb38468041d234c5e4c2801881a93b84c90056
xurst <139590166+xurst@users.noreply.github.com>
2025-02-13 16:50:56 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 const compression = require('compression');
 const helmet = require('helmet');
+const os = require('os');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const getLocalIP = () => {
+    const interfaces = os.networkInterfaces();
+    for (const interfaceName of Object.keys(interfaces)) {
+        for (const interface of interfaces[interfaceName]) {
+            if (!interface.internal && interface.family === 'IPv4') {
+                return interface.address;
+            }
+        }
+    }
+    return 'localhost';
+};
 
 app.use(helmet({
     contentSecurityPolicy: {
@@ -50,13 +63,17 @@ app.use(helmet({
 }));
 
 app.use(compression());
-
 app.use(express.static(path.join(__dirname, './')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    const localIP = getLocalIP();
+    console.log('\n=== mobile debug server running ===');
+    console.log(`local url: http://localhost:${PORT}`);
+    console.log(`network url: http://${localIP}:${PORT}`);
+    console.log('\nuse the network url for mobile device testing');
+    console.log('=====================================');
 });
